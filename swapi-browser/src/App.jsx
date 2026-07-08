168205f5034eca8ee163689ae468d8ed9fb39ad8
wshepelak-catalyte <wshepelak@catalyte.io>
2026-07-08 16:01:00 -0500

    Add swapi-browser React + Vite app
    Add a new swapi-browser React + Vite project scaffold. Includes package.json + lockfile, vite.config.js, index.html, ESLint config, .gitignore, public assets (favicon/icons), src files (App.jsx, main.jsx, index.css, App.css, assets), and a README. Sets up React 19, Vite, and linting plugins; App.jsx contains a placeholder comment for API logic. Intended as the initial app/template for the SWAPI character browser.

---
+++ swapi-browser/src/App.jsx
@@ -0,0 +1,6 @@
+/*
+    This is where are main api logic needs to happen
+*/
+
+import React, {useState, useEffect} from "react";
+
---
+++ swapi-browser/src/main.jsx
@@ -0,0 +1,14 @@
+/*
+  This is just here to run our app and not much else
+*/
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
