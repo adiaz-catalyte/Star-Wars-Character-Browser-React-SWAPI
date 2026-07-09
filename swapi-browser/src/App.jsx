9c72dafe3bc80e9683138d4dbd8c5ad1458a9f9e
wshepelak-catalyte <wshepelak@catalyte.io>
2026-07-09 09:33:05 -0500

    Add test character and CharacterTable in App.jsx
    Introduce fetch_characters.js exporting a test_object (Luke Skywalker) and implement a CharecterTable component in App.jsx that imports the test object and renders a simple characters table. App.jsx now exports default App which mounts the table. Note: the test_object contains a typo ('heigt') while the table reads 'height', which may cause the height column to be empty.

--- swapi-browser/src/App.jsx
+++ swapi-browser/src/App.jsx
@@ -1,6 +1,52 @@
 /*
     This is where are main api logic needs to happen
 */
-
 import React, {useState, useEffect} from "react";
+import { test_object } from "./fetch_characters";
 
+
+function CharecterTable() {
+
+    const characters = [test_object];
+
+    return (
+        <div style={{ padding: '20px' }}>
+            <h2>Character Table</h2>
+            <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '10px'}}>
+                {/* Table Headers */}
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Height</th>
+                        <th>Mass</th>
+                        <th>Birth Year</th>
+                        <th>Gender</th>               
+                    </tr>
+                </thead>
+
+                {/* Table Body with Dynamic Rows */}
+                <tbody>
+                    {characters.map((character) => (
+                        <tr>
+                            <td>{character.name}</td>
+                            <td>{character.height}</td>
+                            <td>{character.mass}</td>
+                            <td>{character.birth_year}</td>
+                            <td>{character.gender}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <div>
+            <CharecterTable />
+        </div>
+    )
+}
+
+export default App;
---
+++ swapi-browser/src/fetch_characters.js
@@ -0,0 +1,15 @@
+export const test_object = {
+    "name": "Luke Skywalker",
+    "heigt": "172",
+    "mass": "77",
+    "hair_color": "blond",
+    "skin_color": "fair",
+    "eye_color": "blue",
+    "birth_year": "19BBY",
+    "gender": "male",
+    "homeworld": "https://swapi.tech/api/planets/1",
+    "films": ["https://swapi.tech/api/films/1"],
+    "url": "https://swapi.tech/api/people/1"
+};
+
+
